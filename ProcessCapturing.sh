c8e5ba1ce859c725d46e30e309872e318f79a69d
AnushMkrtchyan888 <anush.mkrtchyan.itc@gmail.com>
2019-10-10 11:51:36 +0400

    Created bash script for capturing the Linux processes date and adding in the files by process type. Added ReadMe file.

---
+++ ProcessCapturing.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo "Hello, process capturing starts"
+RAV=$(ps -o s,pid,user,%mem,command ax | grep -v PID | sort -bnr -k4 | awk '/[0-9]*/{print $1 " - " $2 " - " $3 "-" $4 " - " $5}')
+
+if [[ -d ~/Processes ]]
+ then
+    echo "Removing the older folder"
+    rm -rf ~/Processes
+fi
+(mkdir ~/Processes)
+(echo "$RAV" > ~/Processes/process.txt )
+
+for i in {1..300}
+do
+STR=$(head -n $i "/home/anush_m/Processes/process.txt" | tail -n +$i)
+if [[ $STR =~ ^S ]]
+then
+(echo "$STR" >>~/Processes/Stopped.txt)
+fi
+if [[ $STR =~ ^R ]]
+then
+(echo "$STR" >>~/Processes/Running.txt)
+fi
+if [[ $STR =~ ^I ]]
+then
+(echo "$STR" >>~/Processes/Interrupt.txt)
+fi
+if [[ $STR =~ ^D ]]
+then
+(echo "$STR" >>~/Processes/Daemons.txt)
+fi
+if [[ $STR =~ ^Z ]]
+then
+(echo "$STR" >>~/Processes/Zombie.txt)
+fi
+if [[ $STR =~ ^W ]]
+then
+(echo "$STR" >>~/exam/Waiting.txt)
+fi	
+done
+echo "Processes added in the /processType.txt/ fails from the Processes folder "
+
---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+Description
+The bash script for capturing the Linux processes date and adding in the files by process type.
+
+Requirements
+Linux operating system
+
+Do the following step before
+
+$. git clone https://github.com/...... 
+Run automated script
+$. open terminal 
+$. cd to the ProcessCapturing.sh
+
+To run the script you need to write the run command provided below.
+$. run the script with ./ProcessCapturing.sh command
+
+To run the script every day(minute, hour, month, year) automatically you need to add crontab command.
+$ open terminal
+$ run the crontab -e command
+$ switch to the Insert mode and go to the end of the opened document
+$ set the crontab command for running on a regular schedule and save
+
+The results you can see in the ~/Processes folder. Processes added in the *.txt fils by process type. All processes added in the process.txt file.
